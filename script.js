b9d5ad4e0a677de3953a62cc1849f034fd5d9eb3
Zerithexe <barisamayeisolan@gmail.com>
2026-06-12 10:49:58 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function login(){
+
+    let password = document.getElementById("password").value;
+
+    if(password === "1903"){
+        window.location.href = "home.html";
+    }else{
+        alert("Yanlış şifre! Sadece gerçek Beşiktaşlılar girebilir.");
+    }
+
+}
